d266b995d0ba2e754d1d9e9910be389a85768dfe
RuqayaAbdulhaq <rouqayaabdulhaq@gmail.com>
2023-11-21 01:48:48 +0300

    first draft for styling text input component

--- components/UI/Input/InputText.js
+++ components/UI/Input/InputText.js
@@ -5,10 +5,61 @@ export default function InputText(props) {
    * ? TODO: change the styling of this component to match the design on figma
    */
   return (
-    <div className="text-gray-400 focus-within:text-green-600 mb-10 input-box">
-      <label for={props.name}>{props.label}: </label>
+    <div className="form__group">
+      <style>
+        {`
+          .form__group {
+            position: relative;
+            padding: 15px 0 0;
+            margin-top: 10px;
+          }
+          
+          .form__field {
+            font-family: inherit;
+            width: 100%;
+            border: 0;
+            border-bottom: 1px solid #d2d2d2;
+            outline: 0;
+            font-size: 16px;
+            color: #212121;
+            padding: 7px 0;
+            background: transparent;
+            transition: border-color 0.2s;
+          }
+          
+          .form__field::placeholder {
+            color: transparent;
+          }
+          
+          .form__field:placeholder-shown ~ .form__label {
+            font-size: 16px;
+            cursor: text;
+            top: 20px;
+          }
+          
+          label,
+          .form__field:focus ~ .form__label {
+            position: absolute;
+            top: 0;
+            display: block;
+            transition: 0.2s;
+            font-size: 12px;
+            color: #9b9b9b;
+          }
+          
+          .form__field:focus ~ .form__label {
+            color: #009788;
+          }
+          
+          .form__field:focus {
+            padding-bottom: 6px;
+            border-bottom: 2px solid #009788;
+          }
+        `}
+      </style>
+      <label className="form__label mb-10" htmlFor={props.name}>{props.label}: </label>
       <input
-        className="ml-2 px-4 py-2 border-b bg-gray-100 rounded"
+        className="form__field"
         type="text"
         name={props.name}
       />
